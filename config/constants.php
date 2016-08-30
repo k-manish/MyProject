ae1e8983cd1d9d29bc05248a23b36226b5409320
Manish Kumar <Manish Kumar>
2016-08-30 17:58:40 +0530

    file deleted

--- config/constants.php
+++ config/constants.php
@@ -1,5 +1,15 @@
 <?php
+/**
+* File Name :Constants.php
+* File Path :http://localhost/MyProject/config/
+* Author :Manish Kumar
+* Date of creation :29/8/2016
+* Comments if any : constents are  defined.
+*
+*/
+
 define("server","localhost");
 define("user","root");
 define("dbpassword","");
-?>
+define("dbname","demodb");
+
